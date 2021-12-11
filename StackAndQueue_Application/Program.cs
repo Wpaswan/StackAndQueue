e9c9ec6c82266f6fc43d8fe51ec0803a4f1d3e66
Wpaswan <dablumcapaswan@gmail.com>
2021-12-11 01:55:16 -0800

    [I,modified the code to perform pop and peek]

--- StackAndQueue_Application/Program.cs
+++ StackAndQueue_Application/Program.cs
@@ -1,10 +1,13 @@
-﻿
-using StackAndQueue_Application;
-
+﻿using StackAndQueue_Application;
 Console.WriteLine("***********Welcome to Stack and Queue Program**********");
 LinkedListStack linkedListStack = new LinkedListStack();
 linkedListStack.Push(70);
 linkedListStack.Push(30);
 linkedListStack.Push(56);
-
 linkedListStack.Display();
+linkedListStack.Peek();
+linkedListStack.Pop();
+linkedListStack.IsEmpty();
+linkedListStack.Display();
+
+
