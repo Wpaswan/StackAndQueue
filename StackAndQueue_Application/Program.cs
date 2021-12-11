ffb601396794d3d38b7ec24f6aefd999e31fbcad
Wpaswan <dablumcapaswan@gmail.com>
2021-12-11 01:07:32 -0800

    [I,added a code to create Stack]

---
+++ StackAndQueue_Application/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using StackAndQueue_Application;
+
+Console.WriteLine("***********Welcome to Stack and Queue Program**********");
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+
+linkedListStack.Display();
